37f539cbc50b146493fdb3c7f07891ec121d6a7d
NTIV-H <edin.adilagic@ga.ntig.se>
2023-01-12 13:30:14 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'homepage1',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
